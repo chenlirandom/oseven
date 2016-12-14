61c8630df4c8131e793385c8a7c2d779e1601cc6
Chen Li <chenlirandom@gmail.com>
2016-12-13 17:03:48 -0800

    renamed java package name to 07.main

--- src/a/app/src/main/java/o7/main/MainActivity.java
+++ src/a/app/src/main/java/o7/main/MainActivity.java
@@ -1,4 +1,4 @@
-package oseven.o7;
+package o7.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import o7.main.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
