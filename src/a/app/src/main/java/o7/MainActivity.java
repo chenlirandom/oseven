c125a1dbbcb67a48197d1a49f38a86ea5a2590df
Chen Li <chenlirandom@gmail.com>
2016-12-15 01:36:19 -0800

    rename layouts to my like

--- src/a/app/src/main/java/o7/MainActivity.java
+++ src/a/app/src/main/java/o7/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_options, menu);
         return true;
     }
 
